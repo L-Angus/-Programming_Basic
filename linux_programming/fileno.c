2d5a81ed0bc2c281bab283fddf90a2449eb9101c
angus.li <lsd15651008321@163.com>
2022-11-16 20:09:06 +0800

    Update Version

---
+++ linux_programming/fileno.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: fileno.c
+	> Author: angus.li
+	> Mail: lsd15651008321@163.com 
+	> Created Time: 二  9/27 14:51:33 2022
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	printf("fileno(stdin) = %d\n", fileno(stdin));
+	return 0;
+}
